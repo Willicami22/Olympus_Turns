61288cec41733c7fd03b8075c5c8ef1369d67385
Carlos-Avellaneda <carlos.avellaneda-f@mail.escuelaing.edu.co>
2025-04-30 08:03:04 -0500

    Creacion del springbootapp

---
+++ src/test/java/edu/eci/cvds/EciBienestarTotal/ModuloTurnos/ModuloTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.EciBienestarTotal.ModuloTurnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
